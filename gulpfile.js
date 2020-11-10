40ba6e3274aaaa328f93207ab859aaa821cb025d
Crow26ru <7tacson7@gmail.com>
2020-11-10 16:48:46 +0300

    Сохраняет промежуточный вариант сборки

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import plumber from 'gulp-plumber';
+import imgmin from 'gulp-imagemin';
+import autoprefixer from 'autoprefixer';
+import sync from 'browser-sync';
+import del from 'del';
+
+// Tasks
+export const server = () => {
+  sync.init({
+    ui: false,
+    notify: false,
+    server: {
+      baseDir: 'dist'
+    }
+  });
+}
+
+export const copy = () => {
+  return gulp.src([
+      'src/img/*.{jpg,png,webp,svg}',
+      'src/fonts/*.{woff,woff2}'
+    ])
+    .pipe(gulp.dest('dist'));
+}
+
+export const deleteDist = () => {
+  return del('dist');
+}
+
+// Default
+// exports.default;
